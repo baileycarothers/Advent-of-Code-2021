2d3144422882eae302373db51c77b9b781382266
baileycarothers <44585056+baileycarothers@users.noreply.github.com>
2022-01-03 05:34:43 -0600

    day 2

--- Day 2/Problem 1/main.py
+++ Day 2/Problem 1/main.py
@@ -1,7 +1,8 @@
 # Advent of Code 2021 Solution
 # By: Bailey Carothers
 # For: Day 2, Part 1
-# Objective: Read numbers from a file, count how many are higher than the previous
+# Objective: Find displacement of a submarine based on given inputs
+# Inputs are structured as "forward 7"
 # See requirements.txt for full explanation
 
 
@@ -13,11 +14,10 @@ if __name__ == "__main__":
     # Open the file and go line by line
     with open("input.txt", "r") as infile:
         for line in infile:
+            # Split divides strings at whitespace by default, result is a list
             input = line.split()
             if input[0] == "forward":
                 horizontal += int(input[1])
-            elif input[0] == "backward":
-                horizontal -= int(input[1])
             # When modifying our depth, downward movement increases our depth variable
             elif input[0] == "up":
                 depth -= int(input[1])
@@ -34,5 +34,3 @@ if __name__ == "__main__":
     with open("output.txt", "w") as outfile:
         outfile.write(str(finalDistance))
     outfile.close()
-
-
--- Day 2/Problem 2/main.py
+++ Day 2/Problem 2/main.py
@@ -1,7 +1,8 @@
 # Advent of Code 2021 Solution
 # By: Bailey Carothers
-# For: Day 2, Part 1
-# Objective: Read numbers from a file, count how many are higher than the previous
+# For: Day 2, Part 2
+# Objective: Find displacement of a submarine based on given inputs
+# Inputs are structured as "forward 7"
 # See requirements.txt for full explanation
 
 
@@ -9,20 +10,23 @@ if __name__ == "__main__":
     # Declare variables as needed by requirements
     horizontal = 0
     depth = 0
+    aim = 0
 
     # Open the file and go line by line
     with open("input.txt", "r") as infile:
         for line in infile:
+            # Split divides strings at whitespace by default, result is a list
             input = line.split()
+            # Horizontal is modified by forward
+            # Depth is modified by our aim and our directional movement
             if input[0] == "forward":
                 horizontal += int(input[1])
-            elif input[0] == "backward":
-                horizontal -= int(input[1])
-            # When modifying our depth, downward movement increases our depth variable
+                depth += int(input[1]) * aim
+            # Up and down modify our "aim" for the nose of the submarine
             elif input[0] == "up":
-                depth -= int(input[1])
+                aim -= int(input[1])
             elif input[0] == "down":
-                depth += int(input[1])
+                aim += int(input[1])
             else:
                 print("Input file formatted incorrectly")
     infile.close()
@@ -34,5 +38,3 @@ if __name__ == "__main__":
     with open("output.txt", "w") as outfile:
         outfile.write(str(finalDistance))
     outfile.close()
-
-
